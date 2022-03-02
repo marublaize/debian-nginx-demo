7e8f8c986f2fd33a8e7fcfa6958858a809f11114
Thiago Magalhaes <thiagom@doublenines.co>
2022-03-02 20:51:54 -0300

    Fix user-data

--- user-data/user-data.sh
+++ user-data/user-data.sh
@@ -8,8 +8,7 @@ sudo yum update -y
 sudo yum install -y git
 
 # Install Docker
-curl -fsSL -o get-docker.sh https://get.docker.com
-sudo sh get-docker.sh
+sudo amazon-linux-extras install -y docker
 sudo usermod -aG docker $USER
 
 sudo systemctl enable docker.service
@@ -20,12 +19,7 @@ curl -fsSL -o docker-compose https://github.com/docker/compose/releases/latest/d
 sudo install docker-compose /usr/local/bin/
 
 # Clone the repo
-if [[ -d "$DIRECTORY" ]]
-then
-    rm -rf "$DIRECTORY"
-fi
-
 git clone https://github.com/marublaize/debian-nginx-demo.git
 
-cd debian-nginx-demo
-sudo docker-compose up -d
+cd ./debian-nginx-demo
+sudo $(which docker-compose) up -d
