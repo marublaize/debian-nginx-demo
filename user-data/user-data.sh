7273cc5c1344983ade16e092917ea918521d605d
Thiago Magalhaes <2737119+marublaize@users.noreply.github.com>
2022-03-02 20:23:46 -0300

    Update user-data.sh

--- user-data/user-data.sh
+++ user-data/user-data.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
+set -e
+
+export DIRECTORY=debian-nginx-demo
+
 sudo yum update -y
-sudo yum install -y docker git
+sudo yum install -y git
 
-wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
-sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
-sudo chmod -v +x /usr/local/bin/docker-compose
-
-sudo usermod -a -G docker ec2-user
+# Install Docker
+curl -fsSL -o get-docker.sh https://get.docker.com
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER
 
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
+# Install Compose
+curl -fsSL -o docker-compose https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
+sudo install docker-compose /usr/local/bin/
+
+# Clone the repo
+if [[ -d "$DIRECTORY" ]]
+then
+    rm -rf "$DIRECTORY"
+fi
+
 git clone https://github.com/marublaize/debian-nginx-demo.git
 
 cd debian-nginx-demo
-docker-compose up -d
+sudo docker-compose up -d
